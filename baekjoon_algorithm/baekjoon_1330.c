efbfc7db379f100e3fad69cc66b2ae4a1a02a752
JayG-5 <jg55552758@gmail.com>
2021-10-23 15:57:38 +0900

    1330 solve

---
+++ baekjoon_algorithm/baekjoon_1330.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main()
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  if(a>b)
+  {
+    printf(">");
+  }
+  if(a<b)
+  {
+    printf("<");
+  }
+  if(a==b)
+  {
+    printf("==");
+  }
+}
