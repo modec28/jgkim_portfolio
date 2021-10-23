f448b4dc438b9cd6eb59be275fc0fa73847a878b
JayG-5 <jg55552758@gmail.com>
2021-10-23 15:55:04 +0900

    2588 solve

--- baekjoon_algorithm/baekjoon_2588.c
+++ baekjoon_algorithm/baekjoon_2588.c
@@ -4,9 +4,8 @@ main()
   int a,b;
   scanf("%d",&a);
   scanf("%d",&b);
-  int c = a*b;
   printf("%d\n",a*(b%10));
   printf("%d\n",a*((b/10)%10));
   printf("%d\n",a*(b/100));
-  printf("%d",c);
+  printf("%d",a*b);
 }
